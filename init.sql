0202c8f4e9035054bf75bf487a2343195472ded0
Leila Moran <lmmoran@ncsu.edu>
2021-11-18 01:56:14 -0500

    [Fix] Added answers table to init.sql (#60)
    init.sql: added answers table, fixed questions table alignment
    Fix for #55, closes #54

--- init.sql
+++ init.sql
@@ -39,8 +39,16 @@ CREATE TABLE review_questions (
 
 CREATE TABLE questions (
     guild_id        BIGINT NOT NULL,
-	number          BIGINT NOT NULL,
+    number          BIGINT NOT NULL,
     question        VARCHAR NOT NULL,
-	author_id       BIGINT,
-	msg_id          BIGINT NOT NULL
+    author_id       BIGINT,
+    msg_id          BIGINT NOT NULL
+);
+
+CREATE TABLE answers (
+    guild_id        BIGINT NOT NULL,
+    q_number        BIGINT NOT NULL,
+    answer          VARCHAR NOT NULL,
+    author_id       BIGINT,
+    author_role     VARCHAR NOT NULL
 );
