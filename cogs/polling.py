2999ed69d4b343cc614f03e4b30b23162781b3df
Leila Moran <lmmoran@ncsu.edu>
2021-11-26 02:13:57 -0500

    Refactored polling.py
    * Split poll command into poll, quizpoll commands
    * Added input checks
    * Modified error handlers
    * Added regex findall to handle quizpoll options
    * linted!

--- cogs/polling.py
+++ cogs/polling.py
@@ -1,11 +1,11 @@
-import discord
-import asyncio
-from discord.ext import commands
-import random
-from discord.ext.commands.cooldowns import BucketType
+#TODO: This functionality does . . .
 
+import discord
+from discord.ext import commands
+import re
 
 class Poll(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -16,109 +16,221 @@ class Poll(commands.Cog):
             "\N{REGIONAL INDICATOR SYMBOL LETTER D}",
             "\N{REGIONAL INDICATOR SYMBOL LETTER E}",
             "\N{REGIONAL INDICATOR SYMBOL LETTER F}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER G}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER H}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER I}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER J}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER K}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER L}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER M}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER N}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER O}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER P}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER Q}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER R}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER S}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER T}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER U}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER V}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER W}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER X}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER Y}",
-            "\N{REGIONAL INDICATOR SYMBOL LETTER Z}"
         ]
 
     # parses the title, which should be in between curly brackets ('{ title }')
-    def find_title(self, message):
+    #def find_title(self, message):
         # this is the index of the first character of the title
-        first = message.find('{') + 1
+        #first = message.find('{') + 1
         # index of the last character of the title
-        last = message.find('}')
-        if first == 0 or last == -1:
-            return "Not using the command correctly"
-        return message[first:last]
+        #last = message.find('}')
+
+        #if first == last: # if the character after '{' is '}' ... does not check for whitespace.
+        #    return ""
+
+        #if first == 0 or last == -1:
+        #    return ""
+        #return message[first:last]
 
     # parses the options (recursively), which should be in between square brackets ('[ option n ]')
-    def find_options(self, message, options):
+    #def find_options(self, message, options):
         # first index of the first character of the option
-        first = message.find('[') + 1
+        #first = message.find('[') + 1
         # index of the last character of the title
-        last = message.find(']')
-        if (first == 0 or last == -1):
-            if len(options) < 2:
-                return "Not using the command correctly"
-            else:
-                return options
-        options.append(message[first:last])
-        message = message[last + 1:]
-        return self.find_options(message, options)
+        #last = message.find(']')
+        #if (first == 0 or last == -1):
+        #    if len(options) < 2:
+        #        return "Not using the command correctly"
+        #    else:
+        #        return options
+        #options.append(message[first:last])
+        #message = message[last + 1:]
+        #return self.find_options(message, options)
 
         # @commands.Cog.listener()
 
+
     # @commands.cooldown(2, 60, BucketType.user)
-    @commands.command(name="poll", help = 'Create a reaction poll by typing $poll Question. '
-                                          'Create a multi reaction poll by typing +poll {title} [option 1] [option 2] [option 3]')
-    # Limit how often a command can be used, (num per, seconds, BucketType.default/user/member/guild/channel/role)
-    async def poll(self, ctx):
-        message = ctx.message
-        if not message.author.bot:
-            if message.content.startswith("$poll") or message.content.startswith("poll:") or message.content.startswith(
-                    "Poll:") or message.content.startswith("poll:") or message.content.startswith("Poll:"):
-                messageContent = message.clean_content
-                if messageContent.find("{") == -1:
-                    await message.add_reaction('👍')
-                    await message.add_reaction('👎')
-                    await message.add_reaction('🤷')
-                else:
-                    title = self.find_title(messageContent)
-                    options = self.find_options(messageContent, [])
+    # -----------------------------------------------------------------------------------------------------------------
+    #    Function: quizpoll(self, ctx)
+    #    Description: TODO
+    #    Inputs:
+    #       - ctx: context of the command
+    #       -
+    #    Outputs:
+    #       - TODO
+    # -----------------------------------------------------------------------------------------------------------------
+    @commands.command(
+        name="quizpoll",
+        help = 'Create a multi reaction poll by typing $poll "title" [option 1] [option 2] [option 3]... with up to 6 options. \n '
+                'Be sure to enclose title with quotes and options with brackets!\n'
+                'EX: $quizpoll "I am a poll" [Vote for me!] [I am option 2]')
+    async def quizpoll(self, ctx, title: str, *, ops):
+        #message = ctx.message
+        #messageContent = message.clean_content
 
-                    try:
-                        pollMessage = ""
-                        i = 0
-                        for choice in options:
-                            if not options[i] == "":
-                                if len(options) > 21:
-                                    await message.channel.send(
-                                        "Please make sure you are using the command correctly and have less than 21 options.")
-                                    return
-                                elif not i == len(options):
-                                    pollMessage = pollMessage + "\n\n" + self.emojiLetters[i] + " " + choice
-                            i += 1
+        #title = self.find_title(messageContent)
+        #options = self.find_options(messageContent, [])
 
-                        ads = [""]
+        # if title is blank, whitespace only, or just too short!
+        if not title or title.isspace():
+            await ctx.author.send("Please enter a valid title.")
+            await ctx.message.delete()
+            return
 
-                        e = discord.Embed(title="**" + title + "**",
-                                          description=pollMessage + ads[0],
-                                          colour=0x83bae3)
-                        pollMessage = await message.channel.send(embed=e)
-                        i = 0
-                        final_options = []  # There is a better way to do this for sure, but it also works that way
-                        for choice in options:
-                            if not i == len(options) and not options[i] == "":
-                                final_options.append(choice)
-                                await pollMessage.add_reaction(self.emojiLetters[i])
-                            i += 1
-                    except KeyError:
-                        return "Please make sure you are using the format 'poll: {title} [Option1] [Option2] [Option 3]'"
-            else:
-                return
+        if len(title) <= 2:
+            await ctx.author.send("Title too short.")
+            await ctx.message.delete()
+            return
 
+
+        options = re.findall(r'\[([^[\]]*)\]', ops)
+
+        if len(options) < 2:
+            await ctx.author.send(
+                "Polls need at least two options.")
+            await ctx.message.delete()
+            return
+
+        if len(options) > 6:
+            await ctx.author.send(
+                "Polls cannot have more than six options.")
+            await ctx.message.delete()
+            return
+
+        try:
+            pollMessage = ""
+            i = 0
+            for choice in options:
+                if not choice or choice.isspace(): # if empty or whitespace only
+                    await ctx.author.send("Options cannot be blank or whitespace only.")
+                    await ctx.message.delete()
+                    return
+                if not i == len(options):
+                    pollMessage = pollMessage + "\n\n" + self.emojiLetters[i] + "   " + choice
+                i += 1
+
+            ads = ["  "]
+
+            e = discord.Embed(title="**" + title + "**",
+                                description=pollMessage + ads[0],
+                                colour=0x83bae3)
+            pollMessage = await ctx.send(embed=e)
+            i = 0
+            #final_options = []  # There is a better way to do this for sure, but it also works that way
+            for choice in options:
+                if not i == len(options) and not options[i] == "":
+                    #final_options.append(choice)
+                    await pollMessage.add_reaction(self.emojiLetters[i])
+                i += 1
+        except KeyError:
+            await ctx.author.send(
+                'To use the quizpoll command, do: $quizpoll "TITLE" [option1] [option2] [option3] ... with up to 6 options. \n '
+                'Be sure to enclose title with quotes and options with brackets!\n'
+                'EX: $quizpoll "I am a poll" [Vote for me!] [I am option 2]')
+            await ctx.message.delete()
+            return
+        # delete user message
+        ctx.message.delete()
+
+    # -----------------------------------------------------------------------------------------------------------------
+    #    Function: quizpoll_error(self, ctx, error)
+    #    Description: prints error message for quizpoll command
+    #    Inputs:
+    #       - ctx: context of the command
+    #       - error: error message
+    #    Outputs:
+    #       - Error details
+    # -----------------------------------------------------------------------------------------------------------------
+    @quizpoll.error
+    async def quizpoll_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.author.send(
+                'To use the quizpoll command, do: $quizpoll "TITLE" [option1] [option2] [option3] ... with up to 6 options. \n '
+                'Be sure to enclose title with quotes and options with brackets!\n'
+                'EX: $quizpoll "I am a poll" [Vote for me!] [I am option 2]')
+        else:
+            await ctx.author.send(error)
+        await ctx.message.delete()
+
+    # -----------------------------------------------------------------------------------------------------------------
+    #    Function: poll(self, ctx)
+    #    Description: TODO
+    #    Inputs:
+    #       - ctx: context of the command
+    #       -
+    #    Outputs:
+    #       - TODO
+    # -----------------------------------------------------------------------------------------------------------------
+    @commands.command(name="poll", help = 'Create a reaction poll by typing $poll QUESTION\n'
+                                            'EX: $poll What do you think about cats?')
+    async def poll(self, ctx, *, pollstr=''):
+
+        if pollstr == '':
+            await ctx.author.send("Please enter a question for your poll.")
+            #await ctx.send(
+                #'To use the poll command, do: $poll QUESTION\n'
+                #'EX: $poll Is this a good idea?')
+            await ctx.message.delete()
+            return
+
+        # if using pollstr:str instead of *; checks for empty and whitespace only strings
+        #if not pollstr or pollstr.isspace():
+        #    await ctx.author.send("Please enter a question for your poll.")
+            #await ctx.send(
+            #    'To use the poll command, do: $poll QUESTION\n'
+            #    'EX: $poll Is this a good idea?')
+            #await ctx.message.delete()
+        #    return
+
+        if len(pollstr) <= 2:
+            await ctx.author.send("Poll question too short.")
+            await ctx.message.delete()
+            return
+
+        # can make it anonymous or not, is anonymous by default.
+        if "instructor" in [y.name.lower() for y in ctx.author.roles]:
+            author = 'Instructor'
+        else:
+            author = 'Student'
+
+        #author = ctx.message.author.id
+        #author_str = (await self.bot.fetch_user(author)).name
+
+        # create a poll, post to channel, and add reactions.
+        #pollmsg = f"**POLL by {author_str}**\n\n{pollstr}\n** **"
+        pollmsg = f"**POLL by {author}**\n\n{pollstr}\n** **"
+        message = await ctx.send(pollmsg)
+
+        #TODO: ADD POLL ID TO DATABASE.
+        #Need to check for deleted IDs when fetching poll results later.
+
+        await message.add_reaction('👍')
+        await message.add_reaction('👎')
+        await message.add_reaction('🤷')
+
+        # delete original message
+        await ctx.message.delete()
+
+    # -----------------------------------------------------------------------------------------------------------------
+    #    Function: poll_error(self, ctx, error)
+    #    Description: prints error message for poll command
+    #    Inputs:
+    #       - ctx: context of the command
+    #       - error: error message
+    #    Outputs:
+    #       - Error details
+    # -----------------------------------------------------------------------------------------------------------------
     @poll.error
     async def poll_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(error)
-
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.author.send(
+                'To use the poll command, do: $poll QUESTION\n'
+                'EX: $poll Is this a good idea?')
+        else:
+            await ctx.author.send(error)
+        await ctx.message.delete()
 
 def setup(bot):
-    bot.add_cog(Poll(bot))
+    n = Poll(bot)
+    bot.add_cog(n)
